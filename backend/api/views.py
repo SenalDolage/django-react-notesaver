bd55812e9dc12c7dbcc3e119a2368bf9d056ec39
SenalDolage <senal.dolage@gmail.com>
2025-07-29 22:51:43 +0800

    Added user registration, note CRUD API endpoints, and JWT authentication routes

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer, NoteSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Note
 
 # Create your views here.
+
+# View to handle GET and POST requests for notes
+class NoteListCreateView(generics.ListCreateAPIView):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    # Override to filter notes by the authenticated user
+    def get_queryset(self):
+        return Note.objects.filter(author=self.request.user)
+
+    # Override to set the author of the note to the authenticated user
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else:
+            print(serializer.errors)
+
+
+# View to handle DELETE requests for notes
+class NoteDelete(generics.DestroyAPIView):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    # Override to filter notes by the authenticated user
+    def get_queryset(self):
+        return Note.objects.filter(author=self.request.user)
+
+
+
+# handle POST requests for user creation.
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
